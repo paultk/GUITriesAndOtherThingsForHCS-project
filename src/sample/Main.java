7e977e8b03bcec72f1a18cda1f77a1f8a9950c55
paul thomas <paultk@student.hist.no>
2016-04-04 16:16:42 +0200

    second commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -30,99 +30,6 @@ import java.util.TreeSet;
 
 public class Main extends Application {
 
-    String savedString = "";
-
-
-    class AutoCompleteTextField extends TextField {
-
-        private final SortedSet<String> entries;
-        /**
-         * The popup used to select an entry.
-         */
-        private ContextMenu entriesPopup;
-
-        /**
-         * Construct a new AutoCompleteTextField.
-         */
-        public AutoCompleteTextField() {
-            super();
-            entries = new TreeSet<>();
-            entriesPopup = new ContextMenu();
-            textProperty().addListener(new ChangeListener<String>() {
-                @Override
-                public void changed(ObservableValue<? extends String> observableValue, String s, String s2) {
-                    if (getText().length() == 0) {
-                        entriesPopup.hide();
-                    } else {
-
-//                        s = old value
-//                        s2 = new value
-
-                        savedString = observableValue.toString();
-                        System.out.println(observableValue.toString());
-
-                        LinkedList<String> searchResult = new LinkedList<>();
-                        searchResult.addAll(entries.subSet(getText(), getText() + Character.MAX_VALUE));
-                        if (entries.size() > 0) {
-                            populatePopup(searchResult);
-                            if (!entriesPopup.isShowing()) {
-                                entriesPopup.show(AutoCompleteTextField.this, Side.BOTTOM, 0, 0);
-                            }
-                        } else {
-                            entriesPopup.hide();
-                        }
-                    }
-                }
-            });
-
-            focusedProperty().addListener(new ChangeListener<Boolean>() {
-                @Override
-                public void changed(ObservableValue<? extends Boolean> observableValue, Boolean aBoolean, Boolean aBoolean2) {
-                    System.out.println(observableValue.toString());
-                    entriesPopup.hide();
-                }
-            });
-
-        }
-
-        /**
-         * Get the existing set of autocomplete entries.
-         *
-         * @return The existing autocomplete entries.
-         */
-        public SortedSet<String> getEntries() {
-            return entries;
-        }
-
-        /**
-         * Populate the entry set with the given search results.  Display is limited to 10 entries, for performance.
-         *
-         * @param searchResult The set of matching strings.
-         */
-        private void populatePopup(List<String> searchResult) {
-            List<CustomMenuItem> menuItems = new LinkedList<>();
-            // If you'd like more entries, modify this line.
-            int maxEntries = 10;
-            int count = Math.min(searchResult.size(), maxEntries);
-            for (int i = 0; i < count; i++) {
-                final String result = searchResult.get(i);
-                Label entryLabel = new Label(result);
-                CustomMenuItem item = new CustomMenuItem(entryLabel, true);
-                item.setOnAction(new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent actionEvent) {
-                        setText(result);
-                        entriesPopup.hide();
-                    }
-                });
-                menuItems.add(item);
-            }
-            entriesPopup.getItems().clear();
-            entriesPopup.getItems().addAll(menuItems);
-
-        }
-    }
-
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -210,51 +117,8 @@ public class Main extends Application {
         gridpane.add(candidatesLbl, 0, 0);
 
 
-        //Second combobox
-        ComboBox comboBox2 = new ComboBox(currentDishes);
-        comboBox2.getSelectionModel().clearSelection();
-
-        comboBox2.valueProperty().addListener(new ChangeListener() {
-            @Override
-            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
-                newDish.add((Dish) newValue);
-            }
-        });
 
 
-        comboBox2.setCellFactory(new Callback<ListView<Dish>, ListCell<Dish>>() {
-
-            @Override
-            public ListCell<Dish> call(ListView<Dish> p) {
-
-                final ListCell<Dish> cell = new ListCell<Dish>() {
-
-                    @Override
-                    protected void updateItem(Dish t, boolean bln) {
-
-                        super.updateItem(t, bln);
-
-                        if (t != null) {
-                            setText(t.toString());
-                        } else {
-                            setText(null);
-                        }
-                    }
-                };
-
-                return cell;
-            }
-        });
-
-
-        comboBox2.setPromptText("choose Dish");
-        VBox vBox2 = new VBox();
-        vBox2.setPadding(new Insets(5, 5, 5, 5));
-        vBox2.setSpacing(5);
-        vBox2.getChildren().addAll(comboBox2, myLabel);
-
-
-        GridPane gridpane2 = new GridPane();
         gridpane.setPadding(new Insets(5));
         gridpane.setHgap(10);
         gridpane.setVgap(10);
@@ -294,7 +158,6 @@ public class Main extends Application {
 //
 
 
-
         SplitPane splitPane2 = new SplitPane();
         splitPane2.setOrientation(Orientation.VERTICAL);
 
@@ -319,7 +182,7 @@ public class Main extends Application {
                 BooleanProperty observable = new SimpleBooleanProperty();
                 observable.addListener(new ChangeListener<Boolean>() {
                     @Override
-                        public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                    public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
                         if (newValue) {
                             newDish.add(item);
                             System.out.print("asdf");
@@ -333,11 +196,8 @@ public class Main extends Application {
         }));
 
 
-
-
         TableView<Dish> table = new TableView<Dish>();
         ObservableList<Dish> data = newDish;
-
 
 
         table.setEditable(true);
@@ -377,7 +237,7 @@ public class Main extends Application {
 
         HBox upper = new HBox(comboBox, listView);
 
-        HBox lower = new HBox(gridpane, removeMenu, vBox2, vbox3);
+        HBox lower = new HBox(gridpane, removeMenu, vbox2, vbox3);
 
         splitPane2.getItems().add(upper);
         splitPane2.getItems().add(lower);
